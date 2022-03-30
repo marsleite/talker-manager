db2e2e6093f10df2ccec65aa562362280a056ffa
Marcelo <marcelo.leite@mercadolivre.com>
2022-03-29 21:39:58 -0300

    configuracao do porjeto em spring

---
+++ src/main/java/com/hymnsz/talkermanager/TalkerManagerApplication.java
@@ -0,0 +1,13 @@
+package com.hymnsz.talkermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkerManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkerManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hymnsz/talkermanager/TalkerManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hymnsz.talkermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkerManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
